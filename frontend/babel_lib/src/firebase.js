95178a6a2c72650c4ff903362c87fee002d96452
William Zheng <official.william.zheng@gmail.com>
2024-10-27 02:48:16 -0400

    media uploader completed

--- frontend/babel_lib/src/firebase.js
+++ frontend/babel_lib/src/firebase.js
@@ -5,12 +5,12 @@ import { getDatabase } from 'firebase/database';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY?.replace(/"/g, ''),
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN?.replace(/"/g, ''),
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID?.replace(/"/g, ''),
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET?.replace(/"/g, ''),
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID?.replace(/"/g, ''),
+  appId: process.env.REACT_APP_FIREBASE_APP_ID?.replace(/"/g, '')
 };
 
 
